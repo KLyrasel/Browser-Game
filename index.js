01634652878543cb6643656081ed250cee261cd2
Klyrasel <KaiLyrasel@outlook.com>
2022-09-26 16:26:50 -0400

    adding new pages and personalizing them

---
+++ index.js
@@ -0,0 +1,7 @@
+// 1. Explain the scenario/setting
+// 2. Have the player make a decision
+// 3. Progress the game based off of character choice
+// 4. Repeat until ending
+
+ System.out.println("Welcome Message")
+    
