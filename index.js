ce87dc476895823f0cbe868c45b60415d6f1fd0d
Klyrasel <KaiLyrasel@outlook.com>
2022-10-03 16:28:10 -0400

    game over

--- index.js
+++ index.js
@@ -3,18 +3,17 @@
 //3.Add click jump event
 //4.Create function so that jump event can repeat instead of stopping after one jump
 //Find parameters of the pumpkin and the ghost
-//Make an if statement saying if the ghost and pumpkin collide with each other, the game is over and display the number of successful jumps 
-//Display loss and score with innerHTML
-//Get game to speed up after certain amount of time 
-var character = document.getElementById("ghost");
-var obstacles = document.getElementById("pumpkin");
+//Make an if statement saying if the ghost and pumpkin collide with each other, the game is over
+
+var ghost = document.getElementById("ghost");
+var pump = document.getElementById("pumpkin");
 var score = document.getElementById("points")
 
 let counter = 0;
 
 document.addEventListener("click", jump);
 function jump() {
-    character.classList.add("movement");
+    ghost.classList.add("movement");
     setTimeout(removeJump,300);
     counter++;
 }
@@ -28,13 +27,13 @@ function scorecount(){
 var infinite = setInterval(scorecount, 100);
 
 function removeJump() {
-    character.classList.remove("movement")
+    ghost.classList.remove("movement")
 }
 
-
-var ghostTop = parseIntwindow.getComputedStyle(ghost).getComputedStyle("top");
-var ghostLeft = parseIntwindow.getComputedStyle(pumpkin).getComputedStyle("left");
-
-if( ghostTop >= 130 && pumpkinLeft < 20 && pumpkinLeft > -20){
-    alert("you lose");
-}
+let collison = setInterval(function(){
+    let ghostTop = parseInt(window.getComputedStyle(ghost).getPropertyValue("top"));
+    console.log(ghostTop);
+    let pumpkinLeft = parseInt(window.getComputedStyle(pump).getPropertyValue("left"));
+if ( ghostTop >= 150 && pumpkinLeft < 50 && pumpkinLeft > 0 ) {
+    alert("Game over");
+} },10 )
