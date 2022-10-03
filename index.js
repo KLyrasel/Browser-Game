d6a0f73031f71be45c4d2e0de310f21f430675a0
Klyrasel <KaiLyrasel@outlook.com>
2022-10-02 20:26:02 -0400

    changes

--- index.js
+++ index.js
@@ -1,26 +1,29 @@
-var character = document.getElementById("character");
-var obstacles = document.getElementById("obstacles");
-//declares our variables 
+//1.Define variables
+//2.Define counter
+//3.Add click jump event
+//4.Create function so that jump event can repeat instead of stopping after one jump
+//Find parameters of the pumpkin and the cat
+//Make an if statement saying if the cat and pumpkin collide with each other, the game is over and display the number of successful jumps 
+//Display loss and score with innerHTML
+var character = document.getElementById("cat");
+var obstacles = document.getElementById("pumpkin");
 
-document.addEventListener("click",jump);
+let counter = 0;
+
+document.addEventListener("click", jump);
 function jump() {
     character.classList.add("movement");
     setTimeout(removeJump,300);
+    counter++;
 }
-//allows us to jump when clicking but times it out so it isn't constant
 function removeJump() {
     character.classList.remove("movement")
 }
-//remove jump keeps us from jumping when we are just sliding 
 
-var lost = setInterval(function(){
-    var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
-var obstacleLeft = parseInt(window.getComputedStyle(obstacles).getPropertyValue("left"));
-//this will figure out if and where the character is hitting the obstacles by finding the values of the certain sides
-//then below we will make an if statement alerting us if the character hits those values that we have died and restarting the game
-if(obstacleLeft<20 && obstacleLeft>0 && characterTop>=130){
-    document.getElementById("loss").innerHTML = "YOU DIED. SCORE: "
+
+var catTop = parseIntwindow.getComputedStyle(cat).getComputedStyle("top");
+var pumpkinLeft = parseIntwindow.getComputedStyle(pumpkin).getComputedStyle("left");
+
+if( catTop >= 150 && pumpkinLeft < 50 && pumpkinLeft > -50){
+    document.getElementById("loss").innerHTML = " YOU LOSE "
 }
-else{("movement")}
-})
-
