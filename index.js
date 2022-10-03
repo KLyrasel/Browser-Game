4758c333f14fb1c3a96efd4cbd1c8ba35f118879
Klyrasel <KaiLyrasel@outlook.com>
2022-10-03 16:47:42 -0400

    finished project, add scores and end game

--- index.js
+++ index.js
@@ -5,12 +5,16 @@
 //Find parameters of the pumpkin and the ghost
 //Make an if statement saying if the ghost and pumpkin collide with each other, the game is over
 
+
+//defines my variables
 var ghost = document.getElementById("ghost");
 var pump = document.getElementById("pumpkin");
 var score = document.getElementById("points")
-
+//creating counter 
 let counter = 0;
 
+
+//adding a click event to get ghost to jump
 document.addEventListener("click", jump);
 function jump() {
     ghost.classList.add("movement");
@@ -18,22 +22,22 @@ function jump() {
     counter++;
 }
 
-
+//making counter go up by 1 each second and displaying it without an alert
 function scorecount(){
     counter = counter + 1;
     score.innerText = "Score: " + counter;
 }
 
 var infinite = setInterval(scorecount, 100);
-
+//removejump function so that you can jump more than once
 function removeJump() {
     ghost.classList.remove("movement")
 }
-
+//collision check every 10 ms comparing the side of the pumpkin and the ghost jump to see if they collide, then ending game if they do 
 let collison = setInterval(function(){
     let ghostTop = parseInt(window.getComputedStyle(ghost).getPropertyValue("top"));
     console.log(ghostTop);
     let pumpkinLeft = parseInt(window.getComputedStyle(pump).getPropertyValue("left"));
 if ( ghostTop >= 150 && pumpkinLeft < 50 && pumpkinLeft > 0 ) {
-    alert("Game over");
+    alert("GAME OVER");
 } },10 )
